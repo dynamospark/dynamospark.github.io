22be2b0dbcf594c26c66c6389285a04bf91e9f77
Administrator <nithishbabu23@gmail.com>
2019-08-30 16:17:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import * as serviceWorker from './serviceWorker';
+
+import MainPage from './pages/main';
+ReactDOM.render(
+<MainPage/>,
+ document.getElementById('root'));
+
+//serviceWorker.unregister();
---
+++ pages/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+export  default class Mainpage extends React.Component {
+render()
+{
+return (
+   <div> 
+     <p>Name</p>
+     <p>College Name</p>
+     <p>year</p>
+     <p>Department</p>
+   </div>
+)
+}
+}
+
